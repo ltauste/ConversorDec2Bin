7a1046e23c7c867a1a01402dce0ab5edd8c9fa5c
ltauste <113985795+ltauste@users.noreply.github.com>
2022-09-24 17:55:24 +0200

    ff

--- dec2bin.py
+++ dec2bin.py
@@ -3,19 +3,21 @@
 #   determinado número de bits.
 # El binario resultante es un string e.g. "101"
 # Se usa la función bin() que transforma e.g. 3 en "0b11".
-# En   esta función se quita el "0b" para dejar el "11"
+# En esta función se quita el "0b" para dejar el "11"
 # ------------------------------------------------------
 def dec2bin(numero_decimal, numero_bits):
     numero_binario = bin(numero_decimal)
     if numero_decimal >= 0:
-            numero_binario = numero_binario[2:len(numero_binario)]  # quita el "0b" del principio
-    
-            while len(numero_binario) < numero_bits:      # añade 0's a la izquierda si hace falta
-                numero_binario = "0" + numero_binario
+        numero_binario = numero_binario[2:len(numero_binario)]  # quita el "0b" del principio
+
+        while len(numero_binario) < numero_bits:      # añade 0's a la izquierda si hace falta
+            numero_binario = "0" + numero_binario
     else:
-        numero_binario = numero_binario[3:len(numero_binario)] #quita el "-0b" del principio
-        while len(numero_binario) < numero_bits:   #añade 1's a la izquierda si hace falta
+        numero_binario = numero_binario[3:len(numero_binario)]  # quita el "-0b" del principio
+
+        while len(numero_binario) < numero_bits:      # añade 1's a la izquierda si hace falta
             numero_binario = "1" + numero_binario
+
     return numero_binario
 
 # ----------------------------------------
@@ -33,4 +35,3 @@ if __name__ == "__main__":
     # Muestra por pantalla el resultado.
     # Para imprimir un entero es necesario convertirlo a string con str()
     print("El numero " + str(numero_decimal) + " es " + numero_binario + " en binario con " + str(numero_bits) + " bits.")
-    ESTO ES UN TEST DE M1
